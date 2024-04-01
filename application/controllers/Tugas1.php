2c010aecb53550e9d4325b21622509daf9dd3a19
Helena08devi <rc3152905@gmail.com>
2024-04-01 14:35:12 +0700

    tugas1

--- application/controllers/Tugas1.php
+++ application/controllers/Tugas1.php
@@ -5,6 +5,6 @@ class Tugas1 extends CI_controller
     {
         echo "<h1>perkenalan</h1><br>";
         echo "<p>Hallo Nama saya HELENA DEVI saya tingal dijl karya sosial komplek bali asri 2 no 5D 
-        olahraga yang saya sukai adala bermain bola volly dan sedikit tidur</p>";
+        olahraga yang saya sukai adala bermain bola volly</p>";
     }
 }
